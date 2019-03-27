a6d84da7e50996747db14900121eaf624b8945c5
FV6117 <48221526+FV6117@users.noreply.github.com>
2019-03-27 14:52:23 -0400

    Add files via upload
    This is the final version for part b

--- groupbb.py
+++ groupbb.py
@@ -2,89 +2,100 @@ import random
 import matplotlib.pyplot as plt
 import math
 
-random1000 = [] # list for distribution of random numbers
+# calculate array of 1000 random numbers from 0 to 1
+rand_thousand = []
 i = 0
-for i in range(1000): # 1000 random num
-    random1000.append(random.random()) # gives each of 1000 numbers a random [0,1)
+for i in range(1000):
+    rand_thousand.append(random.random())
 
-plt.figure(1) # label figure 1
-plt.hist(random1000,10)  # plot with 10 bins
-plt.title('Probability Distribution (10 bins)') # plot title
-plt.show()
-#print(random1000)
-
-plt.figure(2) # label figure 2
-plt.hist(random1000,20) # plot with 20 bins
-plt.title('Probability Distribution (20 bins)') # plot title
-plt.show()
-#print(random1000)
-
-plt.figure(3) # label figure 3
-plt.hist(random1000,50) # plot with 50 bins
-plt.title('Probability Distribution (50 bins)') # plot title
-plt.show()
-#print(random1000)
-
-plt.figure(4) # label figure 4
-plt.hist(random1000,100) # plot with 100 bins
-plt.title('Probability Distribution (100 bins)') # plot title
-plt.show()
-#print(random1000)
-#######################
-
-random1000000 = [] # list for distribution of random numbers
+rand_million = []
 i = 0
-for i in range(1000000):  # 1000000 random num
-    random1000000.append(random.random())  # gives each of 1000 numbers a random [0,1)
+for i in range(1000000):
+    rand_million.append(random.random())
 
-plt.figure(5)  # label figure 5
-plt.hist(random1000000,10) # plot with 10 bins
-plt.title('Probability Distribution (10 bins)') # plot title
+# plot histogram of 1000 number array with 10, 20, 50, and 100 bins
+plt.figure(1)
+plt.hist(rand_thousand, 10)
+plt.title("Uniform Probability Distribution, 1,000 samples in 10 bins")
 plt.show()
-#print(random1000000)
 
-plt.figure(6) # label figure 6
-plt.hist(random1000000,20) # plot with 20 bins
-plt.title('Probability Distribution (20 bins)') # plot title
+plt.figure(2)
+plt.hist(rand_thousand, 20)
+plt.title("Uniform Probability Distribution, 1,000 samples in 20 bins")
 plt.show()
-#print(random1000000)
 
-plt.figure(7) # label figure 7
-plt.hist(random1000000,50) # plot with 50 bins
-plt.title('Probability Distribution (50 bins)') # plot title
+plt.figure(3)
+plt.hist(rand_thousand, 50)
+plt.title("Uniform Probability Distribution, 1,000 samples in 50 bins")
 plt.show()
-#print(random1000000)
 
-plt.figure(8) # label figure 8
-plt.hist(random1000000,100) # plot with 100 bins
-plt.title('Probability Distribution (100 bins)') # plot title
+plt.figure(4)
+plt.hist(rand_thousand, 100)
+plt.title("Uniform Probability Distribution, 1,000 samples in 100 bins")
 plt.show()
-#print(random1000000)
+
+# plot histogram of 1000000 number array with 10, 20, 50, and 100 bins
+plt.figure(5)
+plt.hist(rand_million, 10)
+plt.title("Uniform Probability Distribution, 1,000,000 samples in 10 bins")
+plt.show()
+
+plt.figure(6)
+plt.hist(rand_million, 20)
+plt.title("Uniform Probability Distribution, 1,000,000 samples in 20 bins")
+plt.show()
+
+plt.figure(7)
+plt.hist(rand_million, 50)
+plt.title("Uniform Probability Distribution, 1,000,000 samples in 50 bins")
+plt.show()
+
+plt.figure(8)
+plt.hist(rand_million, 100)
+plt.title("Uniform Probability Distribution, 1,000,000 samples in 100 bins")
+plt.show()
 
 #############################################################################################
 
 # Part b
 
-x_list = []
-y_list = []
-x_final = 1000
+x_list = [] # list for sampled x
 x_initial = 0
-c = 1/(2 * math.pi)**(1/2)
-x_sample = (x_final * random.random())
-for x in range(x_final):
-    x_sample = (x_final * random.random())
-    x_list.append(x_sample)
+c = 1/((2 * math.pi)**(1/2)) # constant for p
+i = 0
+n = 0
 
-for y in range(x_final):
-    y_sample = (random.random())
-    if y_sample <= c * math.exp((-1/2)*(x_sample**2)):
-        y_list.append(y_sample)
-    else:
-         y_list.append(0)
-
+while n < 1000: # sample 1000 x points
+    x_sample = (10*random.random() -5) # sample x point between -5 and 5 points
+    y_sample = c * (random.random()) # sample y point between 0 and 1 points
+    if y_sample <= c * math.exp((-1/2) * (x_sample**2)): # if y is less than p append to list
+        n+=1
+        x_list.append(x_sample)  # append each of these samples to list
 
 plt.figure(9)
-plt.title('Rejection Method')
-plt.plot(x_list,y_list)
+plt.title('Rejection Method 10 bins')
+plt.hist(x_list, 10)
+plt.xlabel('x')
+plt.ylabel('P(x)')
 plt.show()
 
+plt.figure(10)
+plt.title('Rejection Method 20 bins')
+plt.hist(x_list, 20)
+plt.xlabel('x')
+plt.ylabel('P(x)')
+plt.show()
+
+plt.figure(11)
+plt.title('Rejection Method 50 bins')
+plt.hist(x_list, 50)
+plt.xlabel('x')
+plt.ylabel('P(x)')
+plt.show()
+
+plt.figure(12)
+plt.title('Rejection Method 100 bins')
+plt.hist(x_list, 100)
+plt.xlabel('x')
+plt.ylabel('P(x)')
+plt.show()
