8690e2fe408a7c4c6ee49a3c006ec2236cbcc115
ameliadoetsch <doetschamelia@gmail.com>
2019-04-01 15:08:55 -0400

    part 2 expanded to 3D, some questions remain

--- group_project_part_2.py
+++ group_project_part_2.py
@@ -19,18 +19,26 @@ n_arr = np.arange(0, n, 1)
 
 i = 0
 
-# 1D random walk (I assume this can be expanded into 2D??)
+# 2D random walk
 for j in range(m):
     x = 0
+    y = 0
     for i in range(n):
         rand = random.random()
-        if rand <= 0.5:
+        if rand <= 0.25:
             x += -1
-        else:
+        elif rand <= 0.5 and rand > 0.25:
             x += 1
+        elif rand <= 0.75 and rand > 0.5:
+            y += 1
+        else:
+            y += -1
         # at each time step, we average over all the walkers (divide by m at this step to make code more concise)
-        x_avg[i] += x / m
-        x2_avg[i] += x ** 2 / m
+        #does he want us to just find avg(x2) or like the average of the total displacement squared? since it's 2D
+        #d = np.sqrt(x ** 2 + y ** 2)
+        d = x
+        x_avg[i] += d / m
+        x2_avg[i] += d ** 2 / m
 
 # create plot average x v. time, average x2 v. time
 plt.plot(n_arr, x_avg, label="avg x")
