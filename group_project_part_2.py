ece68879787bff82329ae4c6cdaaa941d86a0c07
iowreth <48215832+iowreth@users.noreply.github.com>
2019-04-05 20:05:52 -0400

    Added an extra loop
    Extra loop added to get an average of mean square distances over several iterations. Caution the added loop has extended the time it takes the code to run fully.  It takes about 3-4 minutes for my machine to run the calculation.

--- group_project_part_2.py
+++ group_project_part_2.py
@@ -10,45 +10,48 @@ x2_avg = np.zeros(n)
 n_arr = np.arange(0, n, 1)
 xlist = np.zeros(n)
 ylist = np.zeros(n)
+MSDList = np.zeros(50)
+deca = np.arange(0, 50, 1)
 i = 0
 
-# 2D random walk
-for j in range(m):
-    x = 0
-    y = 0
-    for i in range(n):
-        rand = random.random()
-        if rand <= 0.25:
-            x += -1
-        elif rand <= 0.5 and rand > 0.25:
-            x += 1
-        elif rand <= 0.75 and rand > 0.5:
-            y += 1
-        else:
-            y += -1
-        # at each time step, we average over all the walkers (divide by m at this step to make code more concise)
-        x_avg[i] += x / m
-        x2_avg[i] += x ** 2 / m
+for l in deca:    # 2D random walk
+    for j in range(m):
+        x = 0
+        y = 0
+        for i in range(n):
+            rand = random.random()
+            if rand <= 0.25:
+                x += -1
+            elif rand <= 0.5 and rand > 0.25:
+                x += 1
+            elif rand <= 0.75 and rand > 0.5:
+                y += 1
+            else:
+                y += -1
+            # at each time step, we average over all the walkers (divide by m at this step to make code more concise)
+            x_avg[i] += x / m
+            x2_avg[i] += x ** 2 / m
 
 
-        ## tracking x and y values for diffusion
-        xlist[i] = x
-        ylist[i] = y
+            ## tracking x and y values for diffusion
+            xlist[i] = x
+            ylist[i] = y
 
-## Declare List for the radius and square difference between each position
-rlist = []
-diffsq = []
-for i in n_arr:
-    # calculate radius
-    rlist.append(np.sqrt(xlist[i]**2 + ylist[i]**2))
-    # calculate the diff between steps, square it and multiply by the probability (0.25)
-    diffsq.append(0.25 * (rlist[i]-rlist[i-1])**2)
+    ## Declare List for the radius and square difference between each position
+    rlist = []
+    diffsq = []
+    for i in n_arr:
+        # calculate radius
+        rlist.append(np.sqrt(xlist[i]**2 + ylist[i]**2))
+        # calculate the diff between steps, square it and multiply by the probability (0.25)
+        diffsq.append(0.25 * (rlist[i]-rlist[i-1])**2)
 
-## find the mean of the square diff
-MSD = np.mean(diffsq)
+    ## find the mean of the square diff
+    MSD = np.mean(diffsq)
+    MSDList[l] = MSD
 
 ## Return mean square difference
-print(MSD)
+print(np.mean(MSDList))
 
 
 # create plot average x v. time, average x2 v. time
